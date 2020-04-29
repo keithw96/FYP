612dacad43133e61aa8a83e77f7fcdc2460922ed
Keith <c00205321@itcarlow.ie>
2020-04-29 20:42:24 +0100

    Raspberry pi exe

---
+++ FYP/Build/CMakeFiles/FYP.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/projects/FYP/FYP/Animation.cpp" "/home/pi/projects/FYP/FYP/Build/CMakeFiles/FYP.dir/Animation.cpp.o"
+  "/home/pi/projects/FYP/FYP/Coin.cpp" "/home/pi/projects/FYP/FYP/Build/CMakeFiles/FYP.dir/Coin.cpp.o"
+  "/home/pi/projects/FYP/FYP/Enemy.cpp" "/home/pi/projects/FYP/FYP/Build/CMakeFiles/FYP.dir/Enemy.cpp.o"
+  "/home/pi/projects/FYP/FYP/Game.cpp" "/home/pi/projects/FYP/FYP/Build/CMakeFiles/FYP.dir/Game.cpp.o"
+  "/home/pi/projects/FYP/FYP/Goal.cpp" "/home/pi/projects/FYP/FYP/Build/CMakeFiles/FYP.dir/Goal.cpp.o"
+  "/home/pi/projects/FYP/FYP/Main.cpp" "/home/pi/projects/FYP/FYP/Build/CMakeFiles/FYP.dir/Main.cpp.o"
+  "/home/pi/projects/FYP/FYP/MainMenuScene.cpp" "/home/pi/projects/FYP/FYP/Build/CMakeFiles/FYP.dir/MainMenuScene.cpp.o"
+  "/home/pi/projects/FYP/FYP/MovingPlatform.cpp" "/home/pi/projects/FYP/FYP/Build/CMakeFiles/FYP.dir/MovingPlatform.cpp.o"
+  "/home/pi/projects/FYP/FYP/Pipe.cpp" "/home/pi/projects/FYP/FYP/Build/CMakeFiles/FYP.dir/Pipe.cpp.o"
+  "/home/pi/projects/FYP/FYP/Play.cpp" "/home/pi/projects/FYP/FYP/Build/CMakeFiles/FYP.dir/Play.cpp.o"
+  "/home/pi/projects/FYP/FYP/Player.cpp" "/home/pi/projects/FYP/FYP/Build/CMakeFiles/FYP.dir/Player.cpp.o"
+  "/home/pi/projects/FYP/FYP/QuestionBlock.cpp" "/home/pi/projects/FYP/FYP/Build/CMakeFiles/FYP.dir/QuestionBlock.cpp.o"
+  "/home/pi/projects/FYP/FYP/Tile.cpp" "/home/pi/projects/FYP/FYP/Build/CMakeFiles/FYP.dir/Tile.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ FYP/Build/CMakeFiles/FYP.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FYP.dir/Main.cpp.o"
+  "CMakeFiles/FYP.dir/Game.cpp.o"
+  "CMakeFiles/FYP.dir/Player.cpp.o"
+  "CMakeFiles/FYP.dir/Tile.cpp.o"
+  "CMakeFiles/FYP.dir/MovingPlatform.cpp.o"
+  "CMakeFiles/FYP.dir/Animation.cpp.o"
+  "CMakeFiles/FYP.dir/QuestionBlock.cpp.o"
+  "CMakeFiles/FYP.dir/Coin.cpp.o"
+  "CMakeFiles/FYP.dir/Goal.cpp.o"
+  "CMakeFiles/FYP.dir/Pipe.cpp.o"
+  "CMakeFiles/FYP.dir/Enemy.cpp.o"
+  "CMakeFiles/FYP.dir/Play.cpp.o"
+  "CMakeFiles/FYP.dir/MainMenuScene.cpp.o"
+  "FYP.pdb"
+  "FYP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FYP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
